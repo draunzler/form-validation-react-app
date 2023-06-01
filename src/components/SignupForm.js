8dcd774c8408c81bca09c4f3d4175e4a22f26c91
Draunzler <shadymeister2017@gmail.com>
2023-06-01 18:28:56 +0530

    commit

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react';
 
 const SignupForm = () => {
+    const[values, setValues] = useState({
+        fullname: "",
+        email: "",
+        password: "",
+    });
+    const handleChange = (event) =>{
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        });
+    };
     const handleFormSubmit = (event) =>{
         event.preventDefault();
     };
@@ -13,15 +24,30 @@ const SignupForm = () => {
             <form classname="form-wrapper">
                 <div className="name">
                     <label className="label">Full Name</label>
-                    <input className="input" type="text" />
+                    <input 
+                    className="input" 
+                    type="text" 
+                    name="fullname" 
+                    value={values.fullname}
+                    onChange={handleChange}/>
                 </div>
                 <div className="email">
                     <label className="label">Email</label>
-                    <input className="input" type="email" />
+                    <input 
+                    className="input" 
+                    type="email" 
+                    name="email" 
+                    value={values.email}
+                    onChange={handleChange}/>
                 </div>
                 <div className="password">
                     <label className="label">Password</label>
-                    <input className="input" type="password" />
+                    <input 
+                    className="input" 
+                    type="password" 
+                    name="password" 
+                    value={values.password}
+                    onChange={handleChange}/>
                 </div>
                 <div>
                     <button className="submit" onClick={handleFormSubmit}>Sign Up</button>
