299379a9064081a23d2b4e9aa4ee056cca542ef3
Draunzler <shadymeister2017@gmail.com>
2023-06-01 19:21:28 +0530

    commit

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import SignupForm from "./SignupForm";
+import SignupFormSuccess from './SignupFormSuccess';
 
 const form = () => {
+  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+  const submitForm = () =>{
+    setFormIsSubmitted(true);
+  }
   return (
-    <div>
-        <SignupForm/>
+  <div>
+    {!formIsSubmitted ? (
+      <SignupForm submitForm={submitForm}/>
+      ) : (
+        <SignupFormSuccess />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default form
+export default form;
