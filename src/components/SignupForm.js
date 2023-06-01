ac14858a64c11a36f5be84e66fe8574cf29dde4e
Draunzler <shadymeister2017@gmail.com>
2023-06-01 21:57:10 +0530

    final commit

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,30 +1,9 @@
 import React,{useState, useEffect} from 'react';
-import validation from './validation';
+import useForm from "./useForm";
 
 const SignupForm = ({submitForm}) => {
-    const[values, setValues] = useState({
-        fullname: "",
-        email: "",
-        password: "",
-    });
-    const [errors, setErrors] = useState({});
-    const [dataIsCorrect, setDataIsCorrect] = useState(false);
-    const handleChange = (event) =>{
-        setValues({
-            ...values,
-            [event.target.name]: event.target.value,
-        });
-    };
-    const handleFormSubmit = (event) =>{
-        event.preventDefault();
-        setErrors(validation(values));
-        setDataIsCorrect(true);
-    };
-    useEffect(() =>{
-        if(Object.keys(errors).length === 0 && dataIsCorrect){
-            submitForm(true);
-        }
-    }, [errors]);
+
+    const{handleChange, handleFormSubmit, values, errors} = useForm(submitForm);
   return (
     <div className="container">
         <div className="app-wrapper">
